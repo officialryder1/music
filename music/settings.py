340abc7d5d6436b6f9c646becc8a57eb33175752
victor ken <kennethvic07@gmail.com>
2022-06-20 23:55:54 +0100

    tweak heroku setting again2

--- music/settings.py
+++ music/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-tymm(*)#h*_+hc%awop%n-lma6$#zyerz!)$)s3m2=-qr-%o3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
