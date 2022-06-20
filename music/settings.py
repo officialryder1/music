b7613c5846ece7bda5768ec108d7b2905c98e629
victor ken <kennethvic07@gmail.com>
2022-06-20 11:52:11 +0100

    turn debbug to false

--- music/settings.py
+++ music/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-tymm(*)#h*_+hc%awop%n-lma6$#zyerz!)$)s3m2=-qr-%o3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
