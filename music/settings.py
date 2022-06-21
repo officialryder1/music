426b167424d87580831a28a0e1277175bf7ded85
victor ken <kennethvic07@gmail.com>
2022-06-21 17:19:54 +0100

    commented statics_storage

--- music/settings.py
+++ music/settings.py
@@ -130,6 +130,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
